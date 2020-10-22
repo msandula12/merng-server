dcf3e185de87735215e5eefdf9263e61806c851a
Mike Sandula <msandula@gmail.com>
2020-10-22 16:43:54 -0400

    feat: getPosts sort by latest first

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
