c42699fa399cc34f74faa0ccc9ac1e5060fd4a03
Mike Sandula <msandula@gmail.com>
2020-10-23 17:47:18 -0400

    feat: display posts

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+
+import PostCard from '../PostCard';
 
 function Home() {
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h2>Loading posts...</h2>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: '2rem' }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      commentCount
+      comments {
+        id
+        body
+        createdAt
+        username
+      }
+      likeCount
+      likes {
+        username
+      }
+      createdAt
+      username
+    }
+  }
+`;
 
 export default Home;
