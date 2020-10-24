1ff91d73899e53ccbfb743126d2dfa26c7d25bab
Mike Sandula <msandula@gmail.com>
2020-10-24 10:16:15 -0400

    feat: persist user via localStorage

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,8 +1,24 @@
 import React, { createContext, useReducer } from 'react';
+import jwtDecode from 'jwt-decode';
 
 // Actions
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
+
+const initialState = {
+  user: null,
+};
+
+// Persist logged-in user as long as token is still valid
+if (localStorage.getItem('jwt')) {
+  const decodedToken = jwtDecode(localStorage.getItem('jwt'));
+  // The token is expired, so remove it
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem('jwt');
+  } else {
+    initialState.user = decodedToken;
+  }
+}
 
 const AuthContext = createContext({
   user: null,
@@ -28,16 +44,18 @@ function authReducer(state, action) {
 }
 
 function AuthProvider(props) {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-  const login = (user) => {
+  const login = (userData) => {
+    localStorage.setItem('jwt', userData.token);
     dispatch({
       type: LOGIN,
-      payload: user,
+      payload: userData,
     });
   };
 
   const logout = () => {
+    localStorage.removeItem('jwt');
     dispatch({
       type: LOGOUT,
     });
