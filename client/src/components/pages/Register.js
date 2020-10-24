4fcc87bd4b4bd2be04920ce1e823a5d4178109fe
Mike Sandula <msandula@gmail.com>
2020-10-24 09:00:13 -0400

    feat: show form loading

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -32,7 +32,7 @@ function Register() {
 
   return (
     <div className="form-container">
-      <Form onSubmit={onSubmit} noValidate>
+      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
         <h1 className="page-title">Register</h1>
         <Form.Input
           label="Username"
