85f034769142c72112dee61e35afc60d81f34978
Mike Sandula <msandula@gmail.com>
2020-10-25 13:00:45 -0400

    fix: npm scripts

--- client/src/components/InfoPopup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Popup } from 'semantic-ui-react';
-
-function InfoPopup({ children, content }) {
-  return <Popup content={content} inverted trigger={children} />;
-}
-
-export default InfoPopup;
